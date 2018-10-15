1218f20b08538b0a5c33b5ec60c10b49d4de45fc
Subhrotechinfo <subhro.techinfo@gmail.com>
2018-10-15 14:26:33 +0530

    navigation updated for recipe&shopping-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shoppingApp';
+
+   loadedFeature = 'recipe' ;
+   onNavigate(feature:string){
+      this.loadedFeature = feature;
+   }
 }
